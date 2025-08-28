f529f86aae613be62f2cd3b9412f9295f52d2cda
A-Chughtai <0abdullah.chughtai9@gmail.com>
2025-08-28 15:36:02 +0500

    Exercise 3: pushed correct code again since the branch protection didn't allow for the faulty code to be merged

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-    return a ** b
+    # # return a ** b (this was the faulty code for exercise 3 to check the branch merge safety for the main branch)
+    return a * b
 
 def divide(a, b):
     if b == 0:
